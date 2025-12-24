8ed766543937364293ffa6a15f15bb77979c8966
mhmdheydarii <heidarimohammadhosin@gmail.com>
2025-12-24 05:45:29 -0800

    Add comment for serializer

--- core/todo/api/serializers.py
+++ core/todo/api/serializers.py
@@ -11,11 +11,12 @@ class TaskSerializer(serializers.ModelSerializer):
         fields = ['id','author' ,'task', 'absolut_url', 'start_date', 'end_date']
         read_only_fields = ['author']
 
+    # Create task only for request.user
     def create(self, validated_data):
         validated_data['author'] = User.objects.get(id=self.context.get('request').user.id)
         return super().create(validated_data)
     
-
+    # Link for task detail
     def get_absolut_url(self, obj):
         return self.context['request'].build_absolute_uri(obj.id)
     
--- core/todo/api/views.py
+++ core/todo/api/views.py
@@ -3,14 +3,19 @@ from rest_framework import viewsets
 from todo.models import Task
 from .serializers import TaskSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-
+from .paginations import Pagination
+from rest_framework import filters
 
 class TaskViewSet(viewsets.ModelViewSet):
     
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
+    pagination_class = Pagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['task']
 
+    # Show tasks only for the user who created them 
     def get_queryset(self):
         user = self.request.user
         if user.is_authenticated:
